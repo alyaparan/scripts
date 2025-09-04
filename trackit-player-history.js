6932ca0db4a0eff1f81685cc3233f769d1c0f617
Alik Paranyan <38263731+alyaparan@users.noreply.github.com>
2025-09-05 02:23:14 +0300

    Update and rename trackit.js to trackit-player-history.js

---
+++ trackit-player-history.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         TrackIt
+// @version      1.0
+// @description  Shows players' history
+// @author       Alik PAranyan | @alyaparan
+// @match        https://*.tankionline.com/*
+// @downloadURL 
+// @updateURL 
+// ==/UserScript==
+
+GM_xmlHttpRequest({
+    method: 'GET'
+});
